7aa15406b3a9178349022c7fa6b0bd948dabaafb
Djendo <HoBeOne@MacBook-Pro-de-Djendo.local>
2019-02-08 16:50:21 +0100

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,84 +1,103 @@
-(function() {
+(function()
+{
 
-  var affichage = document.getElementById("affichage").value;
-  var clic = document.getElementById("clic").value;
-  var score = 0;
-  var multiplicateur = 1;
-  var boost = 50;
-  var autoclick;
-  var autoclickStarted=false;
+var affichage = document.getElementById("affichage").value;
+var clic = document.getElementById("clic").value;
+var score = 0;
+var multiplicateur = 1;
+var boost = 50;
+var autoclick;
+var autoclickStarted=false;
+var valueChrono=11;
 
-  function echoMultiplicateur() {document.getElementById("mult").textContent= multiplicateur};
-  function echoAffichage() {document.getElementById("affichage").textContent= score};
+function echoMultiplicateur() {document.getElementById("mult").textContent= multiplicateur};
+function echoAffichage() {document.getElementById("affichage").textContent= score};
+function chrono() {document.getElementById("echo_chrono").textContent= valueChrono};
 
-
-  document.getElementById("clic").addEventListener("click", function() {
-      score = score + multiplicateur;
-      
-             
+document.getElementById("clic").addEventListener("click", function()
+{
+      score = score + multiplicateur;   
       echoAffichage();
       echoMultiplicateur();
       
 /*-----Auto-click gratuit à 200-----
-      if(autoclickStarted==false){
-      if(score >= 200){
-        setInterval(autoClick, 1000);
-        function autoClick() {
-        score = score +1;
-        echoAffichage();        
-        
-        }
-        autoclickStarted=true;
-      }
+	
+      if(autoclickStarted==false)
+      {
+	      if(score >= 200)
+	      {
+	      setInterval(autoClick, 1000);
+		      
+		      function autoClick()
+		      {
+		      score = score +1;
+		      echoAffichage();     
+		      }
+		      
+	      autoclickStarted=true;
+	      
+	      }
       }
 */
-	  
+});
 
-
-  });
-
-
-  document.getElementById("multiplicateur").addEventListener("click", function augmenterMultiplicateur() {
-  if(score >= boost){
-    multiplicateur = multiplicateur + 1;
-    score = score - boost;
-    boost = boost * 2;
-    echoAffichage();
-    echoMultiplicateur();
-    document.getElementById("boostCost").textContent= boost;
-
-
-  }
-  });
+document.getElementById("multiplicateur").addEventListener("click", function augmenterMultiplicateur()
+{
+	if(score >= boost)
+	{
+	multiplicateur = multiplicateur + 1;
+	score = score - boost;
+	boost = boost * 2;
+	echoAffichage();
+	echoMultiplicateur();
+	document.getElementById("boostCost").textContent= boost;
+	}
+});
   
-  document.getElementById("autoclic").addEventListener("click", function() {
-  
-  echoAffichage();
-      if(autoclickStarted==false){
-      if(score >= 10){
-	    score = score - 10;
-        setInterval(autoClick, 1000);
-        function autoClick() {
-        score = score +1;
-        echoAffichage();        
-        
-        }
-        autoclickStarted=true;
-      }
-      }
-
- });
-document.getElementById("bonus").addEventListener("click", function() {
-  
-  		echoAffichage();
-     if (score >= 50){
-	     score = score - 50;
-	     
-     }
-     
-     
-
- });
+ document.getElementById("autoclic").addEventListener("click", function()
+ 	{
+    if(autoclickStarted==false)
+    {
+	    if(score >= 10)
+	    {
+		score = score - 10;
+		setInterval(autoClick, 1000);
+		
+		    function autoClick()
+		    {
+		    score = score +1;
+		    echoAffichage();            
+		    }
+		    
+		    autoclickStarted=true;
+	    }
+    }
+    
+	echoAffichage();
+	
+	});
+ 
+document.getElementById("bonus").addEventListener("click", function()
+	{
+       if(score === 50)
+     	{
+     	 score = score - 50;
+	 	 var x = setInterval(bonus, 1000);
+	        
+	        function bonus()
+	        {
+			valueChrono = valueChrono-1;
+			multiplicateur = multiplicateur * 2;
+			chrono();
+			
+				if(valueChrono == 0)
+				{
+				multiplicateur = multiplicateur / 2;
+				clearInterval(x);
+		        }
+	        }
+		}
+ 	});
 
 
 })();
